6ada10ee6317c8d64a426a5d3f9afac1ce1b8d67
mdcoo5 <mdcoo5@users.noreply.github.com>
2016-09-08 00:13:06 +1000

    Update prog.cpp

--- 1_RPi/OpenCV/Prod/prog.cpp
+++ 1_RPi/OpenCV/Prod/prog.cpp
@@ -1,3 +1,15 @@
+/*
+TRC3000 Group N OpenCV checkpoint 3 code.
+Author:
+Written: 09/2106
+Purpose: To capture an image from a connected webcam and perform colour thresholding,
+noise reduction and location analysis to determine where in frame coloured object is.
+Outputs: outputs left or right values to the Raspberry Pi's bluetooth serial port to be
+read and processed by BBB.
+*/
+//TODO: Convert file read process to separate function/header.
+//TODO: Change output from console to Serial Port.
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
@@ -24,7 +36,7 @@ int main(int argc, char* argv[])
       return -1;
     }
   
-  // Initial opening of threshold value file
+  // Initial opening and read of threshold value file
   std::fstream fs;
   fs.open("thresh_vals.txt", ios::in);
   if(fs.is_open())
@@ -38,6 +50,8 @@ int main(int argc, char* argv[])
       fs.close();
     }
   else cout << "cannot open file to read threshold vals" << endl;
+  
+  cout << "TRC3000 Group N OpenCV Checkpoint 3." << endl << endl;
 
   // Console output of read threshold values
   cout << "H: " << iLowH;
@@ -45,12 +59,10 @@ int main(int argc, char* argv[])
   cout << "S: " << iLowS;
   cout << " - " << iHighS << endl;
   cout << "V: " <<  iLowV;
-  cout << " - " << iHighV << endl;
-
-  cout << "TRC3000 Group N OpenCV Checkpoint 3." << endl << endl;
-  
+  cout << " - " << iHighV << endl << endl;
 
   //Prompting of user for mode control
+  cout << "Options:" << endl;
   cout << "1 - Set threshold values" << endl;
   cout << "2 - use existing threshold values" << endl << endl;;
   int choice;
@@ -58,22 +70,22 @@ int main(int argc, char* argv[])
   cin >> choice;
   switch(choice)
     {
-    case 1:
+    case 1: //Threshold control
       ctrl_win = 1; out_thrs = 1; out_orig = 0; output = 0;
       cout << "Set final Threshold values in thresh_vals.txt" << endl;
       break;
-    case 2:
+    case 2: //Run with set thresholds
       ctrl_win = 0; out_thrs = 1; out_orig = 0; output = 1;
       break;
-    case 3:
+    case 3: //debug
       ctrl_win = 1; out_thrs = 1; out_orig = 1; output = 1;
       break;
-    default:
+    default: //default errors out
       cout << "Wrong choice" << endl;
       return -1;
     }
   
-  if(ctrl_win == 1)
+  if(ctrl_win == 1) //Opens threshold control window
     {
       namedWindow("Control", CV_WINDOW_AUTOSIZE);
   
@@ -90,10 +102,11 @@ int main(int argc, char* argv[])
   
   while(true)
     {
+      // imgOriginal is img captured from camera
       Mat imgOriginal;
-
       bool bSuccess = cap.read(imgOriginal);
 
+      // Get size of image
       if(flag == 0)
 	{
 	  int rows = imgOriginal.rows;
@@ -108,22 +121,27 @@ int main(int argc, char* argv[])
 	  cout << "Cannot read from camera" << endl;
 	  break;
 	}
-
+ 
+      // Colour converted image
       Mat imgHSV;
 
       cvtColor(imgOriginal, imgHSV, CV_BGR2HSV);
 
+      // binary threshold image and BGR final image
       Mat imgThreshold;
       Mat imgFinal;
 
+      // Treshold image based on values read from file
       inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThreshold);
       
+      // Noise reduction via open/close
       erode(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
       dilate(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
 
       dilate(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
       erode(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
 
+      // Moments function to find centre of blob
       Moments oMoments = moments(imgThreshold);
 
       double dM01 = oMoments.m01;
@@ -136,16 +154,16 @@ int main(int argc, char* argv[])
 	{
 	  posX = dM10 / dArea;
 	  posY = dM01 / dArea;
+	  // Draw red circle on outputted image at centre of 'mass'
 	  circle(imgFinal, Point(posX, posY), 5, Scalar(0, 0, 255), -1, 8);
 	}
 
+      // Draw line down centre of image to denote left/right side
       line(imgFinal, Point(319, 0), Point(319, 479), Scalar(0, 255, 0), 2, 8);
-
 
       if(out_thrs == 1) imshow("Thresholded Image",imgFinal);
       if(out_orig == 1) imshow("Original", imgOriginal);
-
-  
+      
       if(waitKey(30) == 27)
 	{
 	  cout << "esc key pressed" << endl;
@@ -160,7 +178,6 @@ int main(int argc, char* argv[])
 	{
 	  cout << "Right" << endl;
 	}
-
     }
   return 0;
 }
