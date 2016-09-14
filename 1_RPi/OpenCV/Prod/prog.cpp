a6ab92e5999ffcfb00751606896baace305749f3
Matthew Cook <coquus@gmail.com>
2016-09-14 15:56:57 +1000

    thing

--- 1_RPi/OpenCV/Prod/prog.cpp
+++ 1_RPi/OpenCV/Prod/prog.cpp
@@ -14,6 +14,18 @@ read and processed by BBB.
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BAUDRATE B9600
+#define DEVICE "/dev/rfcomm0"
+#define FALSE 0
+#define TRUE 1
 
 using namespace cv;
 using namespace std;
@@ -30,12 +42,33 @@ int iLowV, iHighV;
 int main(int argc, char* argv[])
 {
   VideoCapture cap(0); // captures from camera 0
+  int fd;
+  struct termios oldtio, newtio;
+  
   if(!cap.isOpened())
     {
       cout << "Cannot open the camera" << endl;
       return -1;
     }
-  
+
+  fd = open(DEVICE, O_RDWR | O_NOCTTY | O_NDELAY );
+  if (fd < 0) { cout << "Error opening device" << endl; return -1; }
+  else cout << "Device opened successfully" << endl;
+
+  unsigned char left[] = "left\r";
+  unsigned char right[] = "right\r";
+
+  tcgetattr(fd, &oldtio); // save current serial port settings
+  bzero(&newtio, sizeof(newtio)); // clear struct to recieve new settings
+
+  newtio.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR | ICRNL;
+  newtio.c_oflag = 0;
+  newtio.c_lflag = ICANON;
+
+  tcflush(fd, TCIFLUSH);
+  tcsetattr(fd, TCSANOW, &newtio);
+
   // Initial opening and read of threshold value file
   std::fstream fs;
   fs.open("thresh_vals.txt", ios::in);
@@ -50,7 +83,7 @@ int main(int argc, char* argv[])
       fs.close();
     }
   else cout << "cannot open file to read threshold vals" << endl;
-  
+
   cout << "TRC3000 Group N OpenCV Checkpoint 3." << endl << endl;
 
   // Console output of read threshold values
@@ -84,11 +117,11 @@ int main(int argc, char* argv[])
       cout << "Wrong choice" << endl;
       return -1;
     }
-  
+
   if(ctrl_win == 1) //Opens threshold control window
     {
       namedWindow("Control", CV_WINDOW_AUTOSIZE);
-  
+
       cvCreateTrackbar("LowH","Control",&iLowH,179);
       cvCreateTrackbar("HighH","Control",&iHighH,179);
       cvCreateTrackbar("LowS","Control",&iLowS,255);
@@ -99,7 +132,7 @@ int main(int argc, char* argv[])
 
   int flag = 0;
   int posX, posY;
-  
+
   while(true)
     {
       // imgOriginal is img captured from camera
@@ -121,7 +154,7 @@ int main(int argc, char* argv[])
 	  cout << "Cannot read from camera" << endl;
 	  break;
 	}
- 
+
       // Colour converted image
       Mat imgHSV;
 
@@ -133,7 +166,7 @@ int main(int argc, char* argv[])
 
       // Treshold image based on values read from file
       inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThreshold);
-      
+
       // Noise reduction via open/close
       erode(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
       dilate(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
@@ -151,33 +184,38 @@ int main(int argc, char* argv[])
       cvtColor(imgThreshold, imgFinal, CV_GRAY2BGR);
 
       if(dArea > 10000)
-	{
-	  posX = dM10 / dArea;
-	  posY = dM01 / dArea;
-	  // Draw red circle on outputted image at centre of 'mass'
-	  circle(imgFinal, Point(posX, posY), 5, Scalar(0, 0, 255), -1, 8);
-	}
+	     {
+	        posX = dM10 / dArea;
+	         posY = dM01 / dArea;
+	          // Draw red circle on outputted image at centre of 'mass'
+	           circle(imgFinal, Point(posX, posY), 5, Scalar(0, 0, 255), -1, 8);
+	          }
 
       // Draw line down centre of image to denote left/right side
       line(imgFinal, Point(319, 0), Point(319, 479), Scalar(0, 255, 0), 2, 8);
 
       if(out_thrs == 1) imshow("Thresholded Image",imgFinal);
       if(out_orig == 1) imshow("Original", imgOriginal);
-      
+
       if(waitKey(30) == 27)
 	{
 	  cout << "esc key pressed" << endl;
 	  break;
 	}
 
+int num = 0;
       if( posX < 319 && output)
 	{
-	  cout << "Left" << endl;
+    num = write(fd,left,sizeof(left)-1);
+    cout << "L Data Sent: " << num << " bytes" << endl;
 	}
       else if(output)
 	{
-	  cout << "Right" << endl;
+    num = write(fd,right,sizeof(right)-1);
+    cout << "R Data Sent: " << num << " bytes" << endl;
 	}
     }
+    tcsetattr(fd, TCSANOW, &oldtio);
+    close(fd);
   return 0;
 }
