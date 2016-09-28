0d56998f9123f55128b77918b59392def6b58b3e
Matthew Cook <mdcoo5@student.monash.edu>
2016-09-28 16:48:31 +1000

    updated stuffs

--- 1_RPi/OpenCV/Test/Position/prog.cpp
+++ 1_RPi/OpenCV/Test/Position/prog.cpp
@@ -7,56 +7,64 @@ noise reduction and location analysis to determine where in frame coloured objec
 Outputs: outputs left or right values to the Raspberry Pi's bluetooth serial port to be
 read and processed by BBB.
 */
-//TODO: Determine Location of object within frame
+//TODO: Make usable for Multiple Objects
 
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <fstream>
+/* ----- Includes ----- */
+#include "opencv2/highgui/highgui.hpp" 	// OpenCV
+#include "opencv2/imgproc/imgproc.hpp" 	// OpenCV
+#include <iostream>						
+#include <fstream>						// File Writing
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
-#include <string.h>
+#include <unistd.h>						// Function declarations
+#include <fcntl.h>						// Needed for Serial
+#include <termios.h>					// Serial control
+#include <string.h>						// Strings
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/* ----- Defines ----- */
 #define BAUDRATE B9600
 #define DEVICE "/dev/rfcomm0"
 #define FALSE 0
 #define TRUE 1
 
+/* ----- Namespaces ----- */
 using namespace cv;
 using namespace std;
 
-bool ctrl_win = 1; // 1 for control box window, 0 otherwise
-bool out_thrs = 1; // 1 outputs threshold window
-bool out_orig = 0; // 1 outputs original window
-bool output = 0;   // 1 turns on left/right output
+/* ----- Booleans ----- */
+bool ctrl_win = 1; 						// 1 for control box window, 0 otherwise
+bool out_thrs = 1; 						// 1 outputs threshold window
+bool out_orig = 0; 						// 1 outputs original window
+bool output = 0;   						// 1 turns on left/right output
 
+/* ------ Global Variables ----- */
 int iLowH, iHighH;
 int iLowS, iHighS;
 int iLowV, iHighV;
 
+/* ----- Main Function ----- */
 int main(int argc, char* argv[])
 {
-  VideoCapture cap(0); // captures from camera 0
-  int fd;
-  struct termios oldtio, newtio;
+  VideoCapture cap(0); 					// captures images from camera 0
+  int fd;								// fd for termios structure
+  struct termios oldtio, newtio;		// structs to store serial port settings
   
-  if(!cap.isOpened())
+  if(!cap.isOpened())					// error handling
     {
       cout << "Cannot open the camera" << endl;
       return -1;
     }
 
+  /* ----- Open Serial Port Located at DEVICE ----- */
   fd = open(DEVICE, O_RDWR | O_NOCTTY | O_NDELAY );
   if (fd < 0) { cout << "Error opening device" << endl; return -1; }
   else cout << "Device opened successfully" << endl;
 
-  tcgetattr(fd, &oldtio); // save current serial port settings
-  bzero(&newtio, sizeof(newtio)); // clear struct to recieve new settings
+  tcgetattr(fd, &oldtio); 				// save current serial port settings
+  bzero(&newtio, sizeof(newtio)); 		// clear struct to recieve new settings
 
+  /* ----- Set new Serial port settings ------ */
   newtio.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
   newtio.c_iflag = IGNPAR | ICRNL;
   newtio.c_oflag = 0;
@@ -65,10 +73,10 @@ int main(int argc, char* argv[])
   tcflush(fd, TCIFLUSH);
   tcsetattr(fd, TCSANOW, &newtio);
 
-  // Initial opening and read of threshold value file
-  std::fstream fs;
-  fs.open("thresh_vals.txt", ios::in);
-  if(fs.is_open())
+  /* ----- Initial opening and read of threshold value file ----- */
+  std::fstream fs;  					// fs for reading from file
+  fs.open("thresh_vals.txt", ios::in);	// open fs
+  if(fs.is_open())						// error handling
     {
       fs >> iLowH;
       fs >> iHighH;
@@ -82,7 +90,7 @@ int main(int argc, char* argv[])
 
   cout << "TRC3000 Group N OpenCV Checkpoint 3." << endl << endl;
 
-  // Console output of read threshold values
+  /* ----- Console output of read threshold values ----- */
   cout << "H: " << iLowH;
   cout << " - " << iHighH << endl;
   cout << "S: " << iLowS;
@@ -90,7 +98,7 @@ int main(int argc, char* argv[])
   cout << "V: " <<  iLowV;
   cout << " - " << iHighV << endl << endl;
 
-  //Prompting of user for mode control
+  /* ----- Prompting of user for mode control ----- */
   cout << "Options:" << endl;
   cout << "1 - Set threshold values" << endl;
   cout << "2 - use existing threshold values" << endl << endl;;
@@ -116,8 +124,9 @@ int main(int argc, char* argv[])
       cout << "Wrong choice" << endl;
       return -1;
     }
-
-  if(ctrl_win == 1) //Opens threshold control window
+  
+  /* ----- Opens threshold control window ----- */
+  if(ctrl_win == 1) 
     {
       namedWindow("Control", CV_WINDOW_AUTOSIZE);
 
@@ -154,32 +163,34 @@ int main(int argc, char* argv[])
 	  break;
 	}
 
-      // Colour converted image
+      /* ----- Colour converted image ----- */
       Mat imgHSV;
-
+      
+	  /* ----- Convert BGR image to HSV ----- */
       cvtColor(imgOriginal, imgHSV, CV_BGR2HSV);
 
-      // binary threshold image and BGR final image
+      /* ----- binary threshold image and BGR final image ----- */
       Mat imgThreshold;
       Mat imgFinal;
 
-      // Treshold image based on values read from file
+      /* ----- Treshold image based on values read from file ----- */
       inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThreshold);
 
-      // Noise reduction via open/close
+      /* ----- Noise reduction via open/close ----- */
       erode(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
       dilate(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
 
       dilate(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
       erode(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
 
-      // Moments function to find centre of blob
+      /* ----- Moments function to find centre of blob ----- */
       Moments oMoments = moments(imgThreshold);
 
       double dM01 = oMoments.m01;
       double dM10 = oMoments.m10;
       double dArea = oMoments.m00;
-
+	  
+	  /* ----- Convert color from Grayscale to BGR ----- */
       cvtColor(imgThreshold, imgFinal, CV_GRAY2BGR);
 
       if(dArea > 10000)
@@ -190,7 +201,7 @@ int main(int argc, char* argv[])
 	           circle(imgFinal, Point(posX, posY), 5, Scalar(0, 0, 255), -1, 8);
 	          }
 
-      // Draw line down centre of image to denote left/right side
+      /* ----- Draw line down centre of image to denote left/right side ----- */
       line(imgFinal, Point(posX, posY), Point(319, 239), Scalar(0, 255, 0), 2, 8);
 
       if(out_thrs == 1) imshow("Thresholded Image",imgFinal);
@@ -201,14 +212,14 @@ int main(int argc, char* argv[])
 	  cout << "esc key pressed" << endl;
 	  break;
 	}
-
+	  /* ----- Output Formatting ----- */
       int num = 0;
       char posi[32];
       num = sprintf(posi, "x: %d , y %d \n", posX, posY);
       num = write(fd, posi, sizeof(posi)-1);
       cout << posi;
     }
-
+  /* ----- Reset Output Port ----- */
   tcsetattr(fd, TCSANOW, &oldtio);
   close(fd);
   return 0;
