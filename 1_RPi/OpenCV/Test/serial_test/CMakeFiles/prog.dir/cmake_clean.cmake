c2e4f4efe820271025e976cc9bf28dda15cb40e1
Matthew Cook <coquus@gmail.com>
2016-10-13 17:38:15 +1100

    xxxx opencv changes

---
+++ 1_RPi/OpenCV/Test/serial_test/CMakeFiles/prog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prog.dir/RPI_TEST_OPENCV.cpp.o"
+  "prog.pdb"
+  "prog"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
